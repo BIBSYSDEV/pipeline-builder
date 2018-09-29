016dd8d7ea3be06359d1a1d30cfce91eac605008
Orestis <orestis.gorgas@gmail.com>
2018-09-30 00:00:56 +0200

    wip: create test stack

--- src/test/java/no/bibsys/PipelineTest.java
+++ src/test/java/no/bibsys/PipelineTest.java
@@ -1,7 +1,20 @@
 package no.bibsys;
 
+import static com.amazonaws.regions.ServiceAbbreviations.CloudFormation;
+
+import com.amazonaws.auth.policy.actions.CloudFormationActions;
+import com.amazonaws.services.cloudformation.AmazonCloudFormation;
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClientBuilder;
+import com.amazonaws.services.cloudformation.model.CreateChangeSetRequest;
+import com.amazonaws.services.cloudformation.model.CreateStackRequest;
+import com.amazonaws.services.cloudformation.model.CreateStackSetRequest;
+import com.amazonaws.services.cloudformation.model.OnFailure;
+import com.amazonaws.services.cloudformation.model.ValidateTemplateRequest;
 import com.amazonaws.services.codepipeline.AWSCodePipeline;
 import com.amazonaws.services.codepipeline.AWSCodePipelineClientBuilder;
+import com.amazonaws.services.codepipeline.model.ActionCategory;
+import com.amazonaws.services.codepipeline.model.ActionDeclaration;
+import com.amazonaws.services.codepipeline.model.ActionTypeId;
 import com.amazonaws.services.codepipeline.model.ArtifactStore;
 import com.amazonaws.services.codepipeline.model.CreatePipelineRequest;
 import com.amazonaws.services.codepipeline.model.DeletePipelineRequest;
@@ -10,7 +23,9 @@ import com.amazonaws.services.codepipeline.model.StageDeclaration;
 import com.amazonaws.services.identitymanagement.model.Role;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import no.bibsys.build.CodeBuild;
 import no.bibsys.role.RoleHelper;
 import no.bibsys.source.GithubSource;
@@ -74,19 +89,61 @@ public class PipelineTest implements EnvUtils {
 
   private List<StageDeclaration> createStageDeclarations(Role role) {
     List<StageDeclaration> stageDeclarations = new ArrayList<>();
+
     GithubSource githubSource=new GithubSource(githubCredentials,role,"emneApp");
     StageDeclaration sourceStage=githubSource.addSourceStage();
     stageDeclarations.add(sourceStage);
+
     StageDeclaration buildStage = codeBuild.addBuildStage(githubSource.getArtifactName());
     stageDeclarations.add(buildStage);
+
+    stageDeclarations.add(testStack(codeBuild,role));
+
     return stageDeclarations;
   }
 
 
 
+  private StageDeclaration testStack(CodeBuild codeBuild,Role role){
+    StageDeclaration stageDeclaration=new StageDeclaration();
+    stageDeclaration.setName("TestStackStage");
+    ActionDeclaration actionDeclaration=new ActionDeclaration();
+    actionDeclaration.setName("TestStack");
+//    actionDeclaration.setRoleArn();
+    ActionTypeId actionTypeId=new ActionTypeId();
+    actionTypeId.setCategory(ActionCategory.Deploy);
+    actionTypeId.setOwner("AWS");
+    actionTypeId.setVersion("1");
+    actionTypeId.setProvider("CloudFormation");
 
 
-  public void deployTest(){
+    Map<String,String> configuration=new HashMap<>();
+    configuration.put("ActionMode","CHANGE_SET_REPLACE");
+    configuration.put("StackName","TestStack");
+    configuration.put("ChangeSetName","TestStageChangeSet");
+    String templatePath=String.format("%s::%s",codeBuild.getOutputArtifactName(),"template-export.yml");
+    configuration.put("TemplatePath",templatePath);
+    actionDeclaration.setConfiguration(configuration);
+
+    actionDeclaration.setActionTypeId(actionTypeId);
+    stageDeclaration.withActions(actionDeclaration);
+    return stageDeclaration;
+
+  }
+
+
+
+  public void deployTestEnvironment(){
+
+
+    AmazonCloudFormation cloudFormation = AmazonCloudFormationClientBuilder.defaultClient();
+    CreateStackSetRequest stackSetRequest=new CreateStackSetRequest();
+//    CreateChangeSetRequest
+    CreateStackRequest createStackRequest=new CreateStackRequest();
+    createStackRequest.withCapabilities("CAPABILITY_IAM");
+    createStackRequest.setOnFailure(OnFailure.ROLLBACK);
+
+
 
   }
 
