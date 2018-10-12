92e479bfe6a47ba75de40c35eb428e0378f93088
Orestis Gkorgkas <og@bibsys.no>
2018-10-12 12:46:50 +0200

    wip: change method from get to post

--- src/main/java/no/bibsys/handler/SimpleHandler.java
+++ src/main/java/no/bibsys/handler/SimpleHandler.java
@@ -2,19 +2,16 @@ package no.bibsys.handler;
 
 
 import java.io.IOException;
-import no.bibsys.handler.requests.BranchRequest;
 
-public class SimpleHandler extends HandlerHelper<BranchRequest,String> {
+public class SimpleHandler extends HandlerHelper<String,String> {
 
 
     public SimpleHandler() {
-        super(BranchRequest.class, String.class);
+        super(String.class, String.class);
     }
 
-    protected String processInput(BranchRequest request) throws IOException {
-//        Application application=new Application();
-//        application.run();
-        return "Hello world";
+    protected String processInput(String request) throws IOException {
+       return request;
 
     }
 
