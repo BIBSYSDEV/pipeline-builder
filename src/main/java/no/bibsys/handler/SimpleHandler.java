661e1ce6462e2e65ef8dc838896fb0553c21de80
Orestis <orestis.gorgas@gmail.com>
2018-10-13 19:03:04 +0200

    wip: show request in logs

--- src/main/java/no/bibsys/handler/SimpleHandler.java
+++ src/main/java/no/bibsys/handler/SimpleHandler.java
@@ -3,17 +3,25 @@ package no.bibsys.handler;
 
 import java.io.IOException;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SimpleHandler extends HandlerHelper<Map,String> {
+public class SimpleHandler extends HandlerHelper<String,String> {
 
 
+    Logger logger=LoggerFactory.getLogger(SimpleHandler.class);
     public SimpleHandler() {
-        super(Map.class, String.class);
+        super(String.class, String.class);
     }
 
-    protected String processInput(Map request) throws IOException {
-        Map<String,Object> input=(Map<String,Object>) request;
-       return input.keySet().toString();
+    protected String processInput(String request) throws IOException {
+
+//        Map<String,Object> input=(Map<String,Object>) request;
+        logger.info(request);
+
+
+
+        return request;
 
     }
 
