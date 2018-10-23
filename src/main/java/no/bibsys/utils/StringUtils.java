9c59933a6f80cbc47076de1c990717f1e1e7ed40
Orestis Gkorgkas <og@bibsys.no>
2018-10-23 10:47:29 +0200

    ref: add javadoc and make maxWorldLength a parameter

--- src/main/java/no/bibsys/utils/StringUtils.java
+++ src/main/java/no/bibsys/utils/StringUtils.java
@@ -8,8 +8,14 @@ import java.util.stream.Collectors;
 public class StringUtils {
 
 
-    private final static  transient int maxLength=10;
-
+    /**
+     * Lowercases the input string, replaces underscores with dashes
+     *
+     *
+     *
+     * @param input The string to be normalized
+     * @return The normalized String
+     */
     public String normalizeString(String input){
         String res= input
             .toLowerCase(Locale.getDefault())
@@ -20,17 +26,27 @@ public class StringUtils {
     }
 
 
-    public String shortNormalizedString(String input){
+    /**
+     * Lowercases the input string, replaces underscores with dashes,
+     * and truncates each word (string between two dashes) to {@code maxWordLength}
+     *
+     *
+     *
+     * @param input The string to be normalized
+     * @param maxWorldLength max number of characters between two dashes
+     * @return a normalized String
+     */
+    public String shortNormalizedString(String input,int maxWorldLength){
         String[] words = normalizeString(input).split("-");
-        int maxnumberOfWords = Math.min(maxLength, words.length);
-        List<String> wordList = Arrays.stream(words).map(this::shorten)
+        int maxnumberOfWords = Math.min(maxWorldLength, words.length);
+        List<String> wordList = Arrays.stream(words).map(word->shorten(word,maxWorldLength))
             .collect(Collectors.toList()).subList(0,maxnumberOfWords);
 
         return String.join("-", wordList);
     }
 
 
-    private String shorten(String word) {
+    private String shorten(String word,int maxLength) {
         int maxIndex = Math.min(word.length(), maxLength);
         return word.substring(0, maxIndex);
     }
