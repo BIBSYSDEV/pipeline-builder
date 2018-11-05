a0b08be44402f02032b75064a18333e81c1ac0be
Orestis Gkorgkas <og@bibsys.no>
2018-11-05 13:15:18 +0100

    feat: test for PublishApi class

---
+++ src/test/java/no/bibsys/handler/requests/PublishApiTest.java
@@ -0,0 +1,40 @@
+package no.bibsys.handler.requests;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import no.bibsys.utils.JsonUtils;
+import org.junit.Test;
+
+public class PublishApiTest {
+
+
+    private String test="{\"owner\":\"OWNER\","
+        + "\"repository\":\"REPOSITORY\","
+        + "\"branch\":\"testBranch\","
+        + "\"stage\":\"test\","
+        + "\"swaggetHubApiKey\":\"apiKey\","
+        + "\"apiVersion\":\"apiVersion\","
+        + "\"apiId\":\"api123\""
+        + "}";
+
+
+    @Test
+    public void BuildPhaseTestShouldBeInitlializedThroughAJsonObjet() throws IOException {
+        ObjectMapper parser= JsonUtils.newJsonParser();
+        PublishApi phase=parser.readValue(test, PublishApi.class);
+        assertThat(phase.getStage(),is(equalTo("test")));
+        assertThat(phase.getOwner(),is(equalTo("OWNER")));
+        assertThat(phase.getRepository(),is(equalTo("REPOSITORY")));
+        assertThat(phase.getBranch(),is(equalTo("testBranch")));
+        assertThat(phase.getSwaggetHubApiKey(),is(equalTo("apiKey")));
+        assertThat(phase.getApiVersion(),is(equalTo("apiVersion")));
+        assertThat(phase.getApiId(),is(equalTo("api123")));
+    }
+
+
+
+}
