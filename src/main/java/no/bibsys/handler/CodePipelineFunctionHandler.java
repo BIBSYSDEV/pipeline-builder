17c7068fe191ba3ddf4cdb9dcf9343837b377413
Orestis Gkorgkas <og@bibsys.no>
2018-11-08 10:50:36 +0100

    ref: delete commented out code

--- src/main/java/no/bibsys/handler/CodePipelineFunctionHandler.java
+++ src/main/java/no/bibsys/handler/CodePipelineFunctionHandler.java
@@ -15,9 +15,9 @@ import java.util.Optional;
 import no.bibsys.handler.requests.CodePipelineEvent;
 import no.bibsys.utils.IoUtils;
 
-public abstract class CodePipelineFunctionHandler<O> extends HandlerTemplate<CodePipelineEvent,O> {
+public abstract class CodePipelineFunctionHandler<O> extends HandlerTemplate<CodePipelineEvent, O> {
 
-    private final transient AWSCodePipeline pipeline=AWSCodePipelineClientBuilder.defaultClient();
+    private final transient AWSCodePipeline pipeline = AWSCodePipelineClientBuilder.defaultClient();
 
     public CodePipelineFunctionHandler() {
         super(CodePipelineEvent.class);
@@ -25,17 +25,18 @@ public abstract class CodePipelineFunctionHandler<O> extends HandlerTemplate<Cod
 
     @Override
     protected final CodePipelineEvent parseInput(InputStream inputStream) throws IOException {
-        String jsonSting= IoUtils.streamToString(inputStream);
+        String jsonSting = IoUtils.streamToString(inputStream);
         System.out.println(jsonSting);
         return CodePipelineEvent.create(jsonSting);
     }
 
 
     @Override
-    protected void writeOutput(CodePipelineEvent input,O output) throws IOException {
+    protected void writeOutput(CodePipelineEvent input, O output) throws IOException {
         String outputString = objectMapper.writeValueAsString(output);
         PutJobSuccessResultRequest success = new PutJobSuccessResultRequest();
-        success.withJobId(input.getId()).withExecutionDetails(new ExecutionDetails().withSummary(outputString));
+        success.withJobId(input.getId())
+            .withExecutionDetails(new ExecutionDetails().withSummary(outputString));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
         writer.write(outputString);
         writer.close();
@@ -44,31 +45,20 @@ public abstract class CodePipelineFunctionHandler<O> extends HandlerTemplate<Cod
     }
 
 
-
     @Override
-    protected void writeFailure(CodePipelineEvent input,Throwable error) throws IOException {
+    protected void writeFailure(CodePipelineEvent input, Throwable error) throws IOException {
         String outputString = Optional.ofNullable(error.getMessage())
             .orElse("Unknown error. Check stacktrace.");
 
-        FailureDetails failureDetails=new FailureDetails().withMessage(outputString)
+        FailureDetails failureDetails = new FailureDetails().withMessage(outputString)
             .withType(FailureType.JobFailed);
-        PutJobFailureResultRequest failure=new PutJobFailureResultRequest()
+        PutJobFailureResultRequest failure = new PutJobFailureResultRequest()
             .withJobId(input.getId()).withFailureDetails(failureDetails);
         pipeline.putJobFailureResult(failure);
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
         writer.write(outputString);
         writer.close();
     }
-
-//    private String createContinuationToken(CodePipelineEvent input) throws JsonProcessingException {
-//        ObjectNode continuationToken = objectMapper.getNodeFactory().objectNode();
-//        continuationToken.put("previous_job_id",input.getId());
-//        return objectMapper.writeValueAsString(continuationToken);
-//
-//    }
-
-
-
 
 
 }
