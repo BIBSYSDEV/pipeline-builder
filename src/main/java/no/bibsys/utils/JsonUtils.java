1e19739dc81ba7a61bc546b765dc61e670052dd3
Orestis Gkorgkas <og@bibsys.no>
2018-11-12 15:02:35 +0100

    feat: parse Yaml

--- src/main/java/no/bibsys/utils/JsonUtils.java
+++ src/main/java/no/bibsys/utils/JsonUtils.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.IOException;
 
 public final class JsonUtils {
@@ -20,6 +21,22 @@ public final class JsonUtils {
         return new ObjectMapper(jsonFactory);
     }
 
+
+
+    public static ObjectMapper newYamlParser(){
+        YAMLFactory factory=new YAMLFactory();
+        return new ObjectMapper(factory);
+    }
+
+
+
+    public static String yamlToJson(String yaml) throws IOException {
+        ObjectMapper yamlParser = newYamlParser();
+        JsonNode root = yamlParser.readTree(yaml);
+        ObjectMapper jsonParser=newJsonParser();
+        return jsonParser.writeValueAsString(root);
+
+    }
 
     public static String removeComments(String jsonWithComments) throws IOException {
         ObjectMapper mapper = newJsonParser();
