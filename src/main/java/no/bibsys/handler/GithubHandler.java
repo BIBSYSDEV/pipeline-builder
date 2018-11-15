6d945939fde43ab1842f0b1cfb0dd504186cfdb6
Orestis Gkorgkas <og@bibsys.no>
2018-11-15 10:51:40 +0100

    wip: read token from Github

--- src/main/java/no/bibsys/handler/GithubHandler.java
+++ src/main/java/no/bibsys/handler/GithubHandler.java
@@ -12,14 +12,23 @@ import no.bibsys.handler.requests.PullRequest;
 import no.bibsys.handler.requests.PushEvent;
 import no.bibsys.handler.requests.RepositoryInfo;
 import no.bibsys.handler.templates.ApiGatewayHandlerTemplate;
+import no.bibsys.secrets.SecretsReader;
 import no.bibsys.utils.Environment;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class GithubHandler extends ApiGatewayHandlerTemplate<String, String> {
 
+    private static String AWS_SECRET_NAME = "AWS_SECRET_NAME";
+    private static String AWS_SECRET_KEY = "AWS_SECRET_KEY";
+
 
     private static final Logger logger = LogManager.getLogger(GithubHandler.class);
+
+    private transient String amazonSecretName;
+    private transient String amazonSecretKey;
+    private Environment environment;
 
 
     public GithubHandler() {
@@ -29,7 +38,9 @@ public class GithubHandler extends ApiGatewayHandlerTemplate<String, String> {
     @Override
     public String processInput(String request, Map<String, String> headers, Context context)
         throws IOException {
+
         Optional<RepositoryInfo> gitEventOpt=parseEvent(request);
+
         String webhookSecurityToken = headers.get("X-Hub-Signature");
         verifySecurityToken(webhookSecurityToken);
         String response="No action";
@@ -101,16 +112,47 @@ public class GithubHandler extends ApiGatewayHandlerTemplate<String, String> {
 
     private boolean verifySecurityToken(String token) throws IOException {
         if (token != null) {
+            String storedKey = readSecretsKey();
+            String hashedKey = DigestUtils.sha1Hex(storedKey);
+            String signature = "sha1=" + hashedKey;
+            System.out.println("Input token:" + token);
+            System.out.println("Signature  :" + signature);
 
-            System.out.println("*************" + token);
-//            SecretsReader secretsReader=new SecretsReader();
-//            String securityToken = secretsReader
-//                .readAuthFromSecrets("githubWebhook", "entity-infrastrucutre");
-//            System.out.println(securityToken)
+
         }
 
         return true;
 
+    }
+
+
+    private void init() {
+        if (environment == null) {
+            environment = new Environment();
+        }
+
+    }
+
+    private void initializeAmazonSecrets() {
+        this.amazonSecretName = environment.readEnvOpt(AWS_SECRET_NAME)
+            .orElseThrow(
+                () -> new IllegalStateException("Missing env variable:" + AWS_SECRET_NAME));
+        this.amazonSecretKey = environment.readEnvOpt(AWS_SECRET_KEY)
+            .orElseThrow(() -> new IllegalStateException("Missing env variable:" + AWS_SECRET_KEY));
+    }
+
+
+    private String readSecretsKey() throws IOException {
+        initializeAmazonSecrets();
+        SecretsReader secretsReader = new SecretsReader();
+        String secretValue = secretsReader.readAuthFromSecrets(amazonSecretName, amazonSecretKey);
+        return secretValue;
+
+    }
+
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
 
