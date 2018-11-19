d83bd5f9906b98ddfc95bd71f9f81061223e2ca5
Orestis <orestis.gorgas@gmail.com>
2018-11-19 20:15:29 +0100

    feat: return the complete Server URL and add documentation.

--- src/main/java/no/bibsys/apigateway/ServerInfo.java
+++ src/main/java/no/bibsys/apigateway/ServerInfo.java
@@ -4,24 +4,44 @@ public class ServerInfo {
 
     private final String serverUrl;
     private final String stage;
-
+    private final static String basePathString = "/{basePath}";
 
     public ServerInfo(String serverUrl, String stage) {
         this.serverUrl = serverUrl;
 
-        if (serverUrl.contains("/{basePath}") && stage.charAt(0) == '/') {
+        if (serverUrl.contains(basePathString) && stage.charAt(0) == '/') {
             this.stage = stage.substring(1);
         } else {
             this.stage = stage;
         }
     }
 
-
+    /**
+     * It return  the Server URL in the form of  https://xxxxxxxx.execute-api.eu-west-1.amazonaws.com/{basePath}.
+     * It is intended for to be used by Swagger and SwaggerHub.
+     *
+     * @return the Server url to use in the OpenApi specification.
+     */
     public String getServerUrl() {
         return serverUrl;
     }
 
+
+    /**
+     * @return The ApiGateway Stage for the API. Swagger will use this value to replace {@code
+     * {basePath}}
+     */
     public String getStage() {
         return stage;
+    }
+
+
+    /**
+     *
+     * @return The Server URL where {@code {basepath}} has been replaced by {@code stage}.
+     *
+     */
+    public String completeServerUrl() {
+        return serverUrl.replace(basePathString, "/" + stage);
     }
 }
