9d9d4ad1bd015b938055ab313925728a61ee3b23
Orestis <orestis.gorgas@gmail.com>
2018-11-19 20:48:38 +0100

    feat: parse Stage fromString

--- src/main/java/no/bibsys/cloudformation/Stage.java
+++ src/main/java/no/bibsys/cloudformation/Stage.java
@@ -1,7 +1,10 @@
 package no.bibsys.cloudformation;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 public enum Stage {
 
@@ -12,6 +15,26 @@ public enum Stage {
     @Override
     public String toString(){
         return this.name().toLowerCase(Locale.getDefault());
+    }
+
+
+    public static Optional<Stage> fromString(String stage) {
+        if (stage.equalsIgnoreCase(FINAL.name())) {
+            return Optional.of(FINAL);
+        } else if (stage.equalsIgnoreCase(TEST.name())) {
+            return Optional.of(TEST);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+
+    public static List<String> listStages() {
+
+        List<String> stages = new ArrayList<>();
+        stages.add(TEST.toString());
+        stages.add(FINAL.toString());
+        return stages;
     }
 
 
