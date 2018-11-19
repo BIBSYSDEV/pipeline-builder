f2b921a6b351ea03834d3c5ff3089f8e067d9287
Orestis <orestis.gorgas@gmail.com>
2018-11-19 01:05:13 +0100

    wip: integration test for route53

--- src/test/java/no/bibsys/lambda/deploy/handlers/Route53UpdaterTest.java
+++ src/test/java/no/bibsys/lambda/deploy/handlers/Route53UpdaterTest.java
@@ -3,6 +3,8 @@ package no.bibsys.lambda.deploy.handlers;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import com.amazonaws.services.route53.model.Change;
 import com.amazonaws.services.route53.model.ChangeAction;
@@ -11,9 +13,28 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import no.bibsys.apigateway.ServerInfo;
 import no.bibsys.cloudformation.Stage;
+import no.bibsys.utils.Environment;
+import no.bibsys.utils.IntegrationTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 public class Route53UpdaterTest {
+
+
+    private final transient Environment environment;
+
+    public Route53UpdaterTest() {
+        environment = Mockito.mock(Environment.class);
+        when(environment.readEnv(anyString())).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return null;
+            }
+        });
+    }
 
 
     @Test
@@ -42,6 +63,14 @@ public class Route53UpdaterTest {
         Change change = request.getChangeBatch().getChanges().get(0);
 
         assertThat(change.getAction(), is(equalTo(ChangeAction.UPSERT.toString())));
+    }
+
+
+    @Test
+    @Category(IntegrationTest.class)
+    public void updateRoute53() {
+        Route53Updater updater = new Route53Updater();
+
     }
 
 
