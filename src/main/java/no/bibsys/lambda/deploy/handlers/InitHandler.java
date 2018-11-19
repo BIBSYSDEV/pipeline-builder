4613c48889b7d7eb0203cabcd5f01a577ab61a35
Orestis <orestis.gorgas@gmail.com>
2018-11-19 20:49:17 +0100

    fix: renable Route53 Updater

--- src/main/java/no/bibsys/lambda/deploy/handlers/InitHandler.java
+++ src/main/java/no/bibsys/lambda/deploy/handlers/InitHandler.java
@@ -1,6 +1,7 @@
 package no.bibsys.lambda.deploy.handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.route53.model.ChangeResourceRecordSetsResult;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Optional;
@@ -12,12 +13,12 @@ public class InitHandler extends CodePipelineFunctionHandlerTemplate<SimpleRespo
 
 
     private transient final SwaggerHubUpdater swaggerHubUpdater;
-//    private final transient Route53Updater route53Updater;
+    private final transient Route53Updater route53Updater;
 
     public InitHandler() throws IOException {
         super();
         this.swaggerHubUpdater = new SwaggerHubUpdater();
-//        this.route53Updater = new Route53Updater();
+        this.route53Updater = new Route53Updater();
     }
 
     @Override
@@ -27,15 +28,15 @@ public class InitHandler extends CodePipelineFunctionHandlerTemplate<SimpleRespo
         System.out.println("Lambda function started");
 
         Optional<String> swaggerUpdateResult = swaggerHubUpdater.updateApiDocumentation();
-//        Optional<ChangeResourceRecordSetsResult> route53UpdateResult = route53Updater
-//            .updateServerUrl();
-//        Optional<String> route53Status = route53UpdateResult
-//            .map(result -> result.getChangeInfo().getStatus());
+        Optional<ChangeResourceRecordSetsResult> route53UpdateResult = route53Updater
+            .updateServerUrl();
+        Optional<String> route53Status = route53UpdateResult
+            .map(result -> result.getChangeInfo().getStatus());
         StringBuilder output = new StringBuilder(20);
         output.append("Swagger:");
         swaggerUpdateResult.ifPresent(s -> output.append(s));
-//        output.append("\nRoute53:");
-//        route53Status.ifPresent(s -> output.append(s));
+        output.append("\nRoute53:");
+        route53Status.ifPresent(s -> output.append(s));
 
         return new SimpleResponse(output.toString());
 
