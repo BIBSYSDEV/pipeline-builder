d86aa7ba1cd633efed3e87e2c7752e0125499d5a
Orestis <orestis.gorgas@gmail.com>
2018-11-25 19:43:16 +0100

    wip: wrong Swaggerhub address

--- src/main/java/no/bibsys/swaggerhub/SwaggerHubUrlFormatter.java
+++ src/main/java/no/bibsys/swaggerhub/SwaggerHubUrlFormatter.java
@@ -5,21 +5,25 @@ import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.Optional;
 import no.bibsys.lambda.deploy.handlers.SwaggerHubInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class SwaggerHubUrlFormatter {
 
+    private final static Logger logger = LoggerFactory.getLogger(SwaggerHubUrlFormatter.class);
     private final static String SWAGGERHUB_RESTAPI_URL = "https://api.swaggerhub.com/apis";
     private final transient URI requestURL;
 
 
     public SwaggerHubUrlFormatter(
-       SwaggerHubInfo swaggerHubInfo,
+        SwaggerHubInfo swaggerHubInfo,
         boolean includeApiVersion,
         Map<String, String> requestParameters)
         throws URISyntaxException {
 
-        this.requestURL = urlFormatter(apiUri(swaggerHubInfo,includeApiVersion),requestParameters);
+        this.requestURL = urlFormatter(apiUri(swaggerHubInfo, includeApiVersion),
+            requestParameters);
 
     }
 
@@ -29,33 +33,40 @@ public class SwaggerHubUrlFormatter {
     }
 
 
-
-
-    private URI apiUri(SwaggerHubInfo info,boolean includeApiVersion) throws URISyntaxException {
+    private URI apiUri(SwaggerHubInfo info, boolean includeApiVersion) throws URISyntaxException {
         if (includeApiVersion) {
-            return new URI(String
+
+            URI uri = new URI(String
                 .format("%s/%s/%s/%s", SWAGGERHUB_RESTAPI_URL, info.getSwaggerOrganization(),
                     info.getApiId(),
                     info.getApiVersion()));
+            if (logger.isInfoEnabled()) {
+                logger.info("SwaggerHub URL:{}", uri);
+            }
+            return uri;
         } else {
-            return new URI(
+            URI uri = new URI(
                 String.format("%s/%s/%s", SWAGGERHUB_RESTAPI_URL, info.getSwaggerOrganization(),
                     info.getApiId()));
+            if (logger.isInfoEnabled()) {
+                logger.info("SwaggerHub URL:{}", uri);
+            }
+            return uri;
         }
 
     }
 
 
-    private URI urlFormatter(URI apiAddress, Map<String,String> requestParameters) {
+    private URI urlFormatter(URI apiAddress, Map<String, String> requestParameters) {
 
         Optional<String> parameterOpt = joinParametersToString(requestParameters);
 
         //remove the last slash if there is any
         String host = apiAddress.toString().replaceAll("/$", "");
         if (parameterOpt.isPresent()) {
-            String parametersString=parameterOpt.get();
+            String parametersString = parameterOpt.get();
             String uriString = String.join("?", host, parametersString);
-            URI uri=URI.create(uriString);
+            URI uri = URI.create(uriString);
             return uri;
         } else {
             return URI.create(host);
@@ -64,15 +75,12 @@ public class SwaggerHubUrlFormatter {
     }
 
 
-
     private Optional<String> joinParametersToString(Map<String, String> parameters) {
         return parameters.entrySet()
             .stream()
             .map(entry -> String.format("%s=%s", entry.getKey(), entry.getValue()))
             .reduce((str1, str2) -> String.join("&", str1, str2));
     }
-
-
 
 
 }
