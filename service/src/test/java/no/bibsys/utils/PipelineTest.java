82fe256a172a54a7067c83b9f12380bd88e08f94
Orestis Gkorgkas <og@bibsys.no>
2019-01-11 10:50:24 +0100

    functions for deleting buckets and logs

--- service/src/test/java/no/bibsys/utils/PipelineTest.java
+++ service/src/test/java/no/bibsys/utils/PipelineTest.java
@@ -1,19 +1,25 @@
 package no.bibsys.utils;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.logs.AWSLogs;
+import com.amazonaws.services.logs.AWSLogsClientBuilder;
+import com.amazonaws.services.logs.model.DeleteLogGroupRequest;
+import com.amazonaws.services.logs.model.DeleteLogStreamRequest;
+import com.amazonaws.services.logs.model.DescribeLogStreamsRequest;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import no.bibsys.aws.Application;
 import no.bibsys.aws.git.github.GithubConf;
+import no.bibsys.aws.utils.stacks.StackWiper;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class PipelineTest {
 
 
-    private String branchName = "master";
-    private String repoName = "authority-registry-infrastructure";
+    private String branchName = "jersey-2-authorizer";
+    private String repoName = "authority-registry";
     private String repoOwner = "BIBSYSDEV";
 
 
@@ -36,6 +42,30 @@ public class PipelineTest {
     private Application initApplication() throws IOException {
         GithubConf githubConf = new GithubConf(repoOwner, repoName, branchName);
         return new Application(githubConf);
+    }
+
+
+
+    @Test
+    @Ignore
+    public void deleteAllBuckets() throws IOException {
+        AmazonS3 client= AmazonS3ClientBuilder.defaultClient();
+        StackWiper stackWiper=new StackWiper(initApplication().getPipelineStackConfiguration());
+        client.listBuckets().stream().forEach(bucket->{
+
+            stackWiper.deleteBucket(bucket.getName());
+        });
+
+    }
+
+
+    @Test
+    @Ignore
+    public void deleteAllLogs(){
+        AWSLogs logs= AWSLogsClientBuilder.defaultClient();
+        logs.describeLogGroups().getLogGroups().stream()
+            .forEach(logGroup -> logs.deleteLogGroup(
+                new DeleteLogGroupRequest().withLogGroupName(logGroup.getLogGroupName())));
     }
 
 
