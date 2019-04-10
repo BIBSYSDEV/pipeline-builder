28f0c92fa18e6bca165868d0df04d3b7c3e40741
Orestis Gkorgkas <og@bibsys.no>
2019-04-10 16:49:46 +0200

    AUTREG-179: Test for GithubReader's readFile method

--- service/src/test/java/no/bibsys/aws/utils/github/GithubReaderTest.java
+++ service/src/test/java/no/bibsys/aws/utils/github/GithubReaderTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.mock;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import no.bibsys.aws.git.github.GithubConf;
@@ -22,6 +23,7 @@ public class GithubReaderTest {
     private static final SecretsReader SECRETS_READER = () -> "secret";
     private static final GithubConf githubConf = new GithubConf(OWNER, REPO, BRANCH,
         SECRETS_READER);
+    public static final Path ARBITRARY_PATH = Paths.get("folder", "folder", "file");
 
     @Test
     public void createUrlFromPathShouldMapPathToValidUrl() {
@@ -33,5 +35,13 @@ public class GithubReaderTest {
         GithubReader githubReader = new GithubReader(githubConf, httpClient);
         String outputUrl = githubReader.createUrl(inputPath);
         assertThat(outputUrl, is(equalTo(expectedUrl)));
+    }
+
+    @Test
+    public void readFileShouldReturnAFileForAValidPath() throws IOException, UnauthorizedException, NotFoundException {
+        CloseableHttpClient httpClient = mock(CloseableHttpClient.class);
+        GithubReader githubReader = new GithubReader(githubConf, httpClient);
+        String expectedString = "File contents";
+        assertThat(githubReader.readFile(ARBITRARY_PATH), is(equalTo(expectedString)));
     }
 }
